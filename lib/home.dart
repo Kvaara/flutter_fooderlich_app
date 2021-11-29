fef6ca434505e76e3762b443e1e74f98e969acf6
Jesse Karnavaara <jesse.karnavaara@edu.lapinamk.fi>
2021-11-30 01:20:53 +0200

    Created a separate home.dart file intented to be used as a homepage for the application. This is to avoid making the code overly complicated in the main.dart file.

---
+++ lib/home.dart
@@ -0,0 +1,30 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Fooderlich',
+          style: Theme.of(context).textTheme.headline6,
+        ),
+      ),
+      body: Center(
+        child: Text(
+          'Let\'s get cooking 👩‍🍳',
+          style: Theme.of(context).textTheme.headline1,
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import "theme/custom_theme.dart";
+import "home.dart";
 
 void main() {
   runApp(const Fooderlich());
@@ -10,27 +11,13 @@ class Fooderlich extends StatelessWidget {
   const Fooderlich({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    // TODO: Apply Home widget
     final ThemeData theme = CustomTheme.dark();
 
     return MaterialApp(
       theme: theme,
       darkTheme: CustomTheme.dark(),
       title: 'Fooderlich',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            'Fooderlich',
-            style: theme.textTheme.headline6,
-          ),
-        ),
-        body: Center(
-          child: Text(
-            'Let\'s get cooking 👩‍🍳',
-            style: theme.textTheme.headline1,
-          ),
-        ),
-      ),
+      home: const Home(),
     );
   }
 }
