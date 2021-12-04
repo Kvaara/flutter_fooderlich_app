617c7f8efa95aeecfac1789464a9e15200ab68dc
Jesse Karnavaara <jesse.karnavaara@edu.lapinamk.fi>
2021-12-04 20:13:50 +0200

    Styled the AuthorCard and CircleImage and added the AuthorCard to the Card2 Widget.

--- lib/author_card.dart
+++ lib/author_card.dart
@@ -1,10 +1,46 @@
 import 'package:flutter/material.dart';
 
+import 'circle_image.dart';
+import 'theme/custom_theme.dart';
+
 class AuthorCard extends StatelessWidget {
-  const AuthorCard({Key? key}) : super(key: key);
+  final String authorName;
+  final String title;
+  final ImageProvider? imageProvider;
+
+  const AuthorCard({
+    Key? key,
+    required this.authorName,
+    required this.title,
+    this.imageProvider,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      padding: const EdgeInsets.all(16),
+      child: Row(
+        children: [
+          CircleImage(
+            imageProvider: imageProvider,
+            imageRadius: 32,
+          ),
+          const SizedBox(width: 8),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                authorName,
+                style: CustomTheme.lightTextTheme.headline2,
+              ),
+              Text(
+                title,
+                style: CustomTheme.lightTextTheme.headline3,
+              )
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
--- lib/card2.dart
+++ lib/card2.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_fooderlich_app/author_card.dart';
 
 class Card2 extends StatelessWidget {
   const Card2({Key? key}) : super(key: key);
@@ -21,7 +22,12 @@ class Card2 extends StatelessWidget {
           ),
         ),
         child: Column(
-          children: [
+          children: const [
+            AuthorCard(
+              authorName: "Mike Katz",
+              title: "Smoothie Connoisseur",
+              imageProvider: AssetImage("assets/author_katz.jpeg"),
+            )
             // Stack(
             //   children: const [
             //     Text("Smoothie"),
