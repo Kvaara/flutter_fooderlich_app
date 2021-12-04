9a67b3788bffcb97da0db1feb4a4116bc0294fd5
Jesse Karnavaara <jesse.karnavaara@edu.lapinamk.fi>
2021-12-04 21:20:24 +0200

    Started implementing the final card, Card3.

---
+++ lib/card3.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class Card3 extends StatelessWidget {
+  const Card3({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        constraints: const BoxConstraints.expand(
+          width: 350,
+          height: 450,
+        ),
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/mag2.png"),
+            fit: BoxFit.cover,
+          ),
+          borderRadius: BorderRadius.all(
+            Radius.circular(10.0),
+          ),
+        ),
+        child: Stack(
+          children: [],
+        ),
+      ),
+    );
+  }
+}
