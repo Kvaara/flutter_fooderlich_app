646db68990f22a2f23bf1aabfb9c12dd1cf89d6a
Jesse Karnavaara <jesse.karnavaara@edu.lapinamk.fi>
2021-12-18 23:56:01 +0200

    Created TodayRecipeListView Widget, which is a horizontal list view containing Card widgets with recipes.

--- lib/screens/explore_screen.dart
+++ lib/screens/explore_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '/widgets/widgets.dart';
 import '/models/models.dart';
 import '/api/mock_fooderlich_service.dart';
 
@@ -15,9 +16,7 @@ class ExploreScreen extends StatelessWidget {
       builder: (context, AsyncSnapshot<ExploreData> snapshot) {
         if (snapshot.connectionState == ConnectionState.done) {
           final recipes = snapshot.data?.todayRecipes ?? [];
-          return const Center(
-            child: Text("Show TodayRecipeListView"),
-          );
+          return TodayRecipeListView(recipes: recipes);
         } else {
           return const Center(
             child: CircularProgressIndicator(),
