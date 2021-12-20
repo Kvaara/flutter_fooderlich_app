cae5dff649a680c030fd5eda864d0124603186e7
Jesse Karnavaara <jesse.karnavaara@edu.lapinamk.fi>
2021-12-20 23:59:06 +0200

    Created a a new screen where all of the recipes will reside. Started creating a common recipe thumbnail Widget, which will be used with a GridView widget later on.

--- lib/home.dart
+++ lib/home.dart
@@ -2,6 +2,7 @@
 import 'package:flutter/material.dart';
 
 import 'screens/explore_screen.dart';
+import 'screens/recipes_screen.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -15,8 +16,7 @@ class _HomeState extends State<Home> {
 
   static List<Widget> pages = <Widget>[
     ExploreScreen(),
-    // TODO: Replace with RecipesScreen
-    Container(color: Colors.green),
+    RecipesScreen(),
     Container(color: Colors.blue),
   ];
 
