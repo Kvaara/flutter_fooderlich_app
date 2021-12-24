602d8fa815a33d9c78741e16df48a6cc94821e17
Jesse Karnavaara <jesse.karnavaara@edu.lapinamk.fi>
2021-12-24 12:08:26 +0200

    GroceryScreen is now a Widget with a Scaffold that has a FAB and the body of buildGroceryScreen inside of it. We can now determine, which content to show to the user. If the user hasn't created any groceries, an EmptyGroceryScreen Widget will be returned. If the user has created groceries to buy, we'll return a GroceryListScreen instead(GroceryListScreen will be created soon).

--- lib/screens/grocery_screen.dart
+++ lib/screens/grocery_screen.dart
@@ -1,12 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import "empty_grocery_screen.dart";
+import '../models/models.dart';
 
 class GroceryScreen extends StatelessWidget {
   const GroceryScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const EmptyGroceryScreen();
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {},
+      ),
+      body: buildGroceryScreen(),
+    );
+  }
+
+  Widget buildGroceryScreen() {
+    // 1
+    return Consumer<GroceryManager>(
+      // 2
+      builder: (context, manager, child) {
+        // 3
+        if (manager.groceryItems.isNotEmpty) {
+          // return GroceryListScreen(manager: manager);
+        } else {
+          // 4
+          return const EmptyGroceryScreen();
+        }
+      },
+    );
   }
 }
