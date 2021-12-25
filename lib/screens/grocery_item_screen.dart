2ed3a8d956c6a8485cc22aeb5e61c7d61251a629
Jesse Karnavaara <jesse.karnavaara@edu.lapinamk.fi>
2021-12-26 01:58:15 +0200

    Implemented a buildImportanceField method, which returns a Column with three ChoiceChip widgets inside a Wrap Widget. Users can use these ChoiceChips to select the importance level of an item.

--- lib/screens/grocery_item_screen.dart
+++ lib/screens/grocery_item_screen.dart
@@ -85,6 +85,8 @@ class _GroceryItemScreenState extends State<GroceryItemScreen> {
         child: ListView(
           children: [
             buildNameField(),
+            const SizedBox(height: 11.0),
+            buildImportanceField(),
           ],
         ),
       ),
@@ -115,6 +117,63 @@ class _GroceryItemScreenState extends State<GroceryItemScreen> {
             ),
           ),
         ),
+      ],
+    );
+  }
+
+  Widget buildImportanceField() {
+    // 1
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        // 2
+        Text(
+          'Importance',
+          style: GoogleFonts.lato(fontSize: 28.0),
+        ),
+        // 3
+        Wrap(
+          spacing: 10.0,
+          children: [
+            // 4
+            ChoiceChip(
+              // 5
+              selectedColor: Colors.black,
+              // 6
+              selected: _importance == Importance.low,
+              label: const Text(
+                'low',
+                style: TextStyle(color: Colors.white),
+              ),
+              // 7
+              onSelected: (selected) {
+                setState(() => _importance = Importance.low);
+              },
+            ),
+            ChoiceChip(
+              selectedColor: Colors.black,
+              selected: _importance == Importance.medium,
+              label: const Text(
+                'medium',
+                style: TextStyle(color: Colors.white),
+              ),
+              onSelected: (selected) {
+                setState(() => _importance = Importance.medium);
+              },
+            ),
+            ChoiceChip(
+              selectedColor: Colors.black,
+              selected: _importance == Importance.high,
+              label: const Text(
+                'high',
+                style: TextStyle(color: Colors.white),
+              ),
+              onSelected: (selected) {
+                setState(() => _importance = Importance.high);
+              },
+            ),
+          ],
+        )
       ],
     );
   }
