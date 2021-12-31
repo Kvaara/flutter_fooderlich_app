688213c519a999dbb6db824d03410397230363e2
Jesse Karnavaara <jesse.karnavaara@edu.lapinamk.fi>
2021-12-31 18:22:36 +0200

    Added an AppStateManager, which extends ChangeNotifier. This makes it possible for us to use this as a Provider state, which can be used anywhere on the Widget tree.

---
+++ lib/models/app_state_manager.dart
@@ -0,0 +1,22 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+// 1
+class FooderlichTab {
+  static const int explore = 0;
+  static const int recipes = 1;
+  static const int toBuy = 2;
+}
+
+class AppStateManager extends ChangeNotifier {
+  bool _initialized = false;
+  bool _loggedIn = false;
+  bool _onboardingComplete = false;
+  int _selectedTab = FooderlichTab.explore;
+
+  bool get isInitialized => _initialized;
+  bool get isLoggedIn => _loggedIn;
+  bool get isOnboardingComplete => _onboardingComplete;
+  int get getSelectedTab => _selectedTab;
+}
