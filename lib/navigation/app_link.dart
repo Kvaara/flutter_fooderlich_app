68dfebf70568aded0190d37383061b80c7e41195
Jesse Karnavaara <jesse.karnavaara@edu.lapinamk.fi>
2022-01-07 04:34:59 +0200

    Created the navigation state object. This is like a middleman between the URL string and app state. The navigation state object is used to transform an URL string to a state and a state to an URL string.

---
+++ lib/navigation/app_link.dart
@@ -0,0 +1,76 @@
+class AppLink {
+  static const String kHomePath = '/home';
+  static const String kOnboardingPath = '/onboarding';
+  static const String kLoginPath = '/login';
+  static const String kProfilePath = '/profile';
+  static const String kItemPath = '/item';
+
+  static const String kTabParam = 'tab';
+  static const String kIdParam = 'id';
+
+  String? location;
+
+  int? currentTab;
+
+  String? itemId;
+
+  AppLink({
+    this.location,
+    this.currentTab,
+    this.itemId,
+  });
+
+  static AppLink fromLocation(String? location) {
+    location = Uri.decodeFull(location ?? '');
+
+    final uri = Uri.parse(location);
+    final params = uri.queryParameters;
+
+    final currentTab = int.tryParse(params[AppLink.kTabParam] ?? '');
+
+    final itemId = params[AppLink.kIdParam] ?? "";
+
+    final link = AppLink(
+      location: uri.path,
+      currentTab: currentTab,
+      itemId: itemId,
+    );
+
+    return link;
+  }
+
+  String toLocation() {
+    String addKeyValPair({
+      required String key,
+      String? value,
+    }) =>
+        value == null ? '' : '${key}=$value&';
+
+    switch (location) {
+      case kLoginPath:
+        return kLoginPath;
+
+      case kOnboardingPath:
+        return kOnboardingPath;
+
+      case kProfilePath:
+        return kProfilePath;
+
+      case kItemPath:
+        var loc = '$kItemPath?';
+        loc += addKeyValPair(
+          key: kIdParam,
+          value: itemId,
+        );
+        return Uri.encodeFull(loc);
+
+      default:
+        var loc = '$kHomePath?';
+        loc += addKeyValPair(
+          key: kTabParam,
+          value: currentTab.toString(),
+        );
+        return Uri.encodeFull(loc);
+    }
+  }
+}
